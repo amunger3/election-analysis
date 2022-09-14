693f782bce91ee7e302b91bc5a7d14e9de4ae91c
Alex Munger <munger.alex@gmail.com>
2022-09-14 19:28:45 -0400

    added PollPy.py

---
+++ PollPy.py
@@ -0,0 +1,12 @@
+# PollPy.py
+
+# Retrieve the data
+
+# 1. The total number of votes cast
+
+# 2. A list of condidates who recieved votes
+
+# 3. The percentage of votes each candidate won
+
+# 4. The total number of votes each cadidate won
+
